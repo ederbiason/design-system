8e4f18bea4d8fac1c53ff1e5d962a6188245eace
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-10-18 15:56:44 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
+import { Ignite } from './pages/Ignite'
 import { SignIn } from './pages/SignIn'
 import './styles/global.css'
 
 export function App() {
   return (
-    <SignIn />
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/ignite-lab" element={<Ignite />} />
+    </Routes>
   )
 }
